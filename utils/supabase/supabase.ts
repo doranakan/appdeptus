7db74cc39ab2f425424fccc31d301826c0eddbb1
doranakan <orlandi.m88@gmail.com>
2024-01-09 21:08:02 +0100

    feat: supabase

---
+++ utils/supabase/supabase.ts
@@ -0,0 +1,18 @@
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '../../appConfig.json'
+
+const supabaseAnonKey = SUPABASE_ANON_KEY
+const supabaseUrl = SUPABASE_URL
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false
+  }
+})
+
+export default supabase
