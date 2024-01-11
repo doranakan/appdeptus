e68fb33826f74795e2a2051803c5a7adf061ddc6
doranakan <orlandi.m88@gmail.com>
2024-01-11 22:18:01 +0100

    chore: tab menu icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { GripVerticalIcon, SettingsIcon } from '@gluestack-ui/themed'
 
 const TabsLayout = () => (
   <Tabs screenOptions={{ headerShown: false }}>
-    <Tabs.Screen name='home' options={{ headerShown: false }} />
-    <Tabs.Screen name='settings' options={{ headerShown: false }} />
+    <Tabs.Screen
+      name='home'
+      options={{
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <GripVerticalIcon color={focused ? '$info500' : '$light400'} />
+        )
+      }}
+    />
+    <Tabs.Screen
+      name='settings'
+      options={{
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <SettingsIcon color={focused ? '$info500' : '$light400'} />
+        )
+      }}
+    />
   </Tabs>
 )
 
