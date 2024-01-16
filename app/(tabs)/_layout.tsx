61e33dce3c7059debef43f559dca4fcdae6d3a28
doranakan <orlandi.m88@gmail.com>
2024-01-16 20:09:27 +0100

    feat: handle expired session

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
-import { Tabs } from 'expo-router'
+import { Tabs, useRouter } from 'expo-router'
 import { GripVerticalIcon, SettingsIcon } from '@gluestack-ui/themed'
+import { useAsyncEffect } from 'ahooks'
+import { supabase } from 'appdeptus/utils'
 
-const TabsLayout = () => (
-  <Tabs screenOptions={{ headerShown: false }} initialRouteName='home'>
-    <Tabs.Screen
-      name='home'
-      options={{
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <GripVerticalIcon color={focused ? '$info500' : '$light400'} />
-        )
-      }}
-    />
-    <Tabs.Screen
-      name='settings'
-      options={{
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <SettingsIcon color={focused ? '$info500' : '$light400'} />
-        )
-      }}
-    />
-  </Tabs>
-)
+const TabsLayout = () => {
+  const router = useRouter()
+
+  useAsyncEffect(async () => {
+    const { data } = await supabase.auth.getSession()
+
+    if (data.session === null) {
+      router.replace('/')
+    }
+  }, [])
+
+  return (
+    <Tabs screenOptions={{ headerShown: false }} initialRouteName='home'>
+      <Tabs.Screen
+        name='home'
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <GripVerticalIcon color={focused ? '$info500' : '$light400'} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <SettingsIcon color={focused ? '$info500' : '$light400'} />
+          )
+        }}
+      />
+    </Tabs>
+  )
+}
 
 export default TabsLayout
