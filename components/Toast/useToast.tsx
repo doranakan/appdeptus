2fc99083ae3ebc97e3cae526e138ce7111b7656d
doranakan <orlandi.m88@gmail.com>
2024-02-10 16:09:45 +0100

    style: toast position and size

--- components/Toast/useToast.tsx
+++ components/Toast/useToast.tsx
@@ -6,6 +6,8 @@ import {
   useToast as useGSToast
 } from '@gluestack-ui/themed'
 import { useCallback } from 'react'
+import { useWindowDimensions } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 type ToastArgs = {
   description: string
@@ -16,14 +18,22 @@ type ToastArgs = {
 const useToast = () => {
   const toast = useGSToast()
 
+  const { bottom } = useSafeAreaInsets()
+
+  const { width } = useWindowDimensions()
+
   const show = useCallback(
     ({ description, placement = 'bottom', title }: ToastArgs) =>
       toast.show({
+        containerStyle: { width },
         placement,
         render: () => (
           <Toast
             action='attention'
-            variant='solid'
+            bgColor='$backgroundLight0'
+            mb={bottom + 24}
+            shadowOpacity={0}
+            variant='accent'
           >
             <VStack space='xs'>
               <ToastTitle>{title}</ToastTitle>
