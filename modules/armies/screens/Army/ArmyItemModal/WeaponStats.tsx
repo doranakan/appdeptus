921bfb221d8ef7e551b556a6ca7a382794f4502a
doranakan <orlandi.m88@gmail.com>
2024-03-04 23:08:10 +0100

    feat: army detail

---
+++ modules/armies/screens/Army/ArmyItemModal/WeaponStats.tsx
@@ -0,0 +1,80 @@
+import { Box, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { type Weapon } from 'appdeptus/models'
+import { uniq } from 'lodash'
+import { useMemo } from 'react'
+
+type WeaponStatsProps = {
+  weapons: Weapon[]
+}
+
+const WeaponStats = ({ weapons }: WeaponStatsProps) => {
+  const statNames = useMemo(() => {
+    const stats = weapons.map(({ stats }) => stats)[0]
+
+    if (stats) {
+      return uniq(
+        Object.keys(stats).map((key) => {
+          switch (key) {
+            case 'bsWs':
+              return 'Bs/Ws'
+            default:
+              return key
+          }
+        })
+      )
+    }
+    return []
+  }, [weapons])
+  return (
+    <VStack gap='$2'>
+      <HStack justifyContent='space-between'>
+        {statNames.map((stat) => (
+          <Box
+            key={stat}
+            width={40}
+          >
+            <Text
+              fontWeight='$bold'
+              size='xs'
+              textAlign='center'
+              textTransform='capitalize'
+            >
+              {stat}
+            </Text>
+          </Box>
+        ))}
+      </HStack>
+      {weapons.map(({ id, name, stats, type }) => (
+        <VStack
+          key={id}
+          gap='$1'
+        >
+          <HStack>
+            <Text size='xs'>{name}</Text>
+          </HStack>
+          <HStack
+            backgroundColor='$backgroundLight0'
+            borderRadius='$md'
+            justifyContent='space-between'
+          >
+            {Object.entries(stats).map(([key, val], index) => (
+              <Box
+                key={`${val}-${index}`}
+                width={40}
+              >
+                <Text
+                  size='sm'
+                  textAlign='center'
+                >
+                  {`${val ?? '-'}${key === 'range' && type === 'ranged' ? '"' : ''}`}
+                </Text>
+              </Box>
+            ))}
+          </HStack>
+        </VStack>
+      ))}
+    </VStack>
+  )
+}
+
+export default WeaponStats
