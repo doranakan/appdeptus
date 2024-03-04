1e0df77d88c28a65df32d2e035778d6cde1b6e09
doranakan <orlandi.m88@gmail.com>
2024-03-04 18:43:49 +0100

    fix: total points counter

--- modules/armies/components/UnitListHeader/Header.tsx
+++ modules/armies/components/UnitListHeader/Header.tsx
@@ -1,7 +1,7 @@
 import { Box, Text } from '@gluestack-ui/themed'
 import { Button } from 'appdeptus/components'
 import { type ArmyForm } from 'appdeptus/models'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { useGetCodexUnitsQuery } from '../../api'
 
@@ -18,9 +18,9 @@ const UnitListHeader = ({
   onSubmit,
   submitTitle
 }: UnitListHeaderProps) => {
-  const { watch } = useFormContext<ArmyForm>()
-
   const { data } = useGetCodexUnitsQuery(codexId)
+
+  const { setValue, watch } = useFormContext<ArmyForm>()
 
   const units = watch('units')
 
@@ -31,8 +31,13 @@ const UnitListHeader = ({
         data?.find(({ id }) => id === unit)?.tiers.find(({ id }) => id === tier)
           ?.points ?? 0
     }
+
     return points
   }, [data, units])
+
+  useEffect(() => {
+    setValue('totalPoints', totalPoints)
+  }, [setValue, totalPoints])
 
   return (
     <Box
