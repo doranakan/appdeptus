2077315545f8951946492c63621795cf27dd5c0f
doranakan <orlandi.m88@gmail.com>
2024-06-20 20:42:36 +0200

    asd

--- components/ArmyIcon/ArmyIcon.tsx
+++ components/ArmyIcon/ArmyIcon.tsx
@@ -1,18 +1,23 @@
 import { Box } from '@gluestack-ui/themed'
+import { config } from 'appdeptus/designSystem'
 import { type CodexName } from 'appdeptus/models'
 import { SvgXml } from 'react-native-svg'
 import { mapCodexNameToIcon } from './mapCodexNameToIcon'
 
 type ArmyIconProps = {
   codexName: CodexName
+  color?: string
   flex?: number
   h?: number
   w?: number
 }
 
-const ArmyIcon = ({ codexName, ...props }: ArmyIconProps) => (
+const ArmyIcon = ({ codexName, color = 'black', ...props }: ArmyIconProps) => (
   <Box {...props}>
-    <SvgXml xml={mapCodexNameToIcon[codexName]} />
+    <SvgXml
+      fill={config.tokens.colors[color]}
+      xml={mapCodexNameToIcon[codexName]}
+    />
   </Box>
 )
 
