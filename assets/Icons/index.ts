3c8d55ff0c79e5ffb210790e12fb556eefbadcf8
doranakan <orlandi.m88@gmail.com>
2024-06-20 20:16:55 +0200

    feat: svg icons

---
+++ assets/Icons/index.ts
@@ -0,0 +1,55 @@
+import adeptaSororitasIcon from './adepta-sororitas.svg'
+import adeptusAstartesIcon from './adeptus-astartes.svg'
+import adeptusMechanicusIcon from './adeptus-mechanicus.svg'
+import aeldariIcon from './aeldari.svg'
+import astraMilitarumIcon from './astra-militarum.svg'
+import blackTemplarsIcon from './black-templars.svg'
+import bloodAngelsIcon from './blood-angels.svg'
+import bloodRavensIcon from './blood-ravens.svg'
+import chaosDaemonsIcon from './chaos-daemons.svg'
+import chaosSpaceMarinesIcon from './chaos-space-marines.svg'
+import darkAngelsIcon from './dark-angels.svg'
+import deathGuardIcon from './death-guard.svg'
+import deathwatchIcon from './deathwatch.svg'
+import drukhariIcon from './drukhari.svg'
+import emperorsChildrenIcon from './emperors-children.svg'
+import genestealerCultsIcon from './genestealer-cults.svg'
+import greyKnightsIcon from './grey-knights.svg'
+import imperialFistsIcon from './imperial-fists.svg'
+import inquisitionIcon from './inquisition.svg'
+import necronsIcon from './necrons.svg'
+import orksIcon from './orks.svg'
+import spaceWolvesIcon from './space-wolves.svg'
+import tauIcon from './tau.svg'
+import thousandSonsIcon from './thousand-sons.svg'
+import tyranidsIcon from './tyranids.svg'
+import worldEatersIcon from './world-eaters.svg'
+
+export {
+  adeptaSororitasIcon,
+  adeptusAstartesIcon,
+  adeptusMechanicusIcon,
+  aeldariIcon,
+  astraMilitarumIcon,
+  blackTemplarsIcon,
+  bloodAngelsIcon,
+  bloodRavensIcon,
+  chaosDaemonsIcon,
+  chaosSpaceMarinesIcon,
+  darkAngelsIcon,
+  deathGuardIcon,
+  deathwatchIcon,
+  drukhariIcon,
+  emperorsChildrenIcon,
+  genestealerCultsIcon,
+  greyKnightsIcon,
+  imperialFistsIcon,
+  inquisitionIcon,
+  necronsIcon,
+  orksIcon,
+  spaceWolvesIcon,
+  tauIcon,
+  thousandSonsIcon,
+  tyranidsIcon,
+  worldEatersIcon
+}
