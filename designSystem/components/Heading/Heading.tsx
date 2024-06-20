db9ed4ef9e08809961c8a8ea3ef2afdef71f4d2b
doranakan <orlandi.m88@gmail.com>
2024-06-20 18:49:50 +0200

    style: design system text components

---
+++ designSystem/components/Heading/Heading.tsx
@@ -0,0 +1,8 @@
+import { styled } from '@gluestack-style/react'
+import { Heading as GSHeading } from '@gluestack-ui/themed'
+
+const Heading = styled(GSHeading, {
+  fontFamily: 'Grenze'
+})
+
+export default Heading
---
+++ designSystem/components/Text/Text.tsx
@@ -0,0 +1,8 @@
+import { styled } from '@gluestack-style/react'
+import { Text as GSText } from '@gluestack-ui/themed'
+
+const Text = styled(GSText, {
+  fontFamily: 'NotoSerif'
+})
+
+export default Text
