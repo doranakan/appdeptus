eeb10fce03371808d5103f8c19ff9e947835fc91
doranakan <orlandi.m88@gmail.com>
2024-06-22 13:15:17 +0200

    style: add gradient to card component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,18 +1,31 @@
-import { VStack, type HStack } from '@gluestack-ui/themed'
+import { LinearGradient, VStack, type HStack } from '@gluestack-ui/themed'
+import { LinearGradient as ExpoLinearGradient } from 'expo-linear-gradient'
 import { type PropsWithChildren } from 'react'
 
-type CardProps = (typeof HStack)['defaultProps']
+type CardProps = (typeof HStack)['defaultProps'] & {
+  gradient?: 'primary' | 'secondary'
+}
 
-const Card = ({ children, ...props }: PropsWithChildren<CardProps>) => (
-  <VStack
-    backgroundColor='$secondary100'
-    borderColor='$secondary200'
-    borderWidth='$1'
-    p='$2'
-    {...props}
+const Card = ({
+  children,
+  gradient = 'secondary',
+  ...props
+}: PropsWithChildren<CardProps>) => (
+  <LinearGradient
+    p={1}
+    colors={[`$${gradient}200`, `$${gradient}400`, `$${gradient}100`]}
+    start={0}
+    end={1}
+    as={ExpoLinearGradient}
   >
-    {children}
-  </VStack>
+    <VStack
+      bg='$secondary100'
+      p='$2'
+      {...props}
+    >
+      {children}
+    </VStack>
+  </LinearGradient>
 )
 
 export default Card
