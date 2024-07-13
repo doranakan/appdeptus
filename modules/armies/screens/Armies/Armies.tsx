adfc54643819bf1a0495b17002260a3008f189de
doranakan <orlandi.m88@gmail.com>
2024-07-13 19:33:23 +0200

    refactor: extract army tint effect hook

--- modules/armies/screens/Armies/Armies.tsx
+++ modules/armies/screens/Armies/Armies.tsx
@@ -1,27 +1,15 @@
 import { VStack } from '@gluestack-ui/themed'
 import { Loading } from 'appdeptus/components'
-import { setColorMode } from 'appdeptus/designSystem'
-import { useFocusEffect } from 'expo-router'
-import { useCallback } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { useDispatch } from 'react-redux'
 import { useGetArmiesQuery } from '../../api'
 import ArmyList from './ArmyList'
 import Background from './Background'
 import Header from './Header'
 
 const ArmiesScreen = () => {
-  const dispatch = useDispatch()
-
   const insets = useSafeAreaInsets()
 
   const { data } = useGetArmiesQuery()
-
-  useFocusEffect(
-    useCallback(() => {
-      dispatch(setColorMode('light'))
-    }, [dispatch])
-  )
 
   if (!data) {
     return <Loading />
