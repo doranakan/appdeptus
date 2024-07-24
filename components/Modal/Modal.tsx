91e9986f9799d7c5297b8d8ae01ea559bcd2e212
doranakan <orlandi.m88@gmail.com>
2024-07-25 00:51:37 +0200

    fix: modal header 🙆🏼

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,9 +1,9 @@
-import { HStack, Heading, Icon, Pressable, VStack } from '@gluestack-ui/themed'
-import { Link } from 'expo-router'
+import { VStack } from '@gluestack-ui/themed'
 import { X } from 'lucide-react-native'
 import { type PropsWithChildren } from 'react'
 import { Platform } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Header from '../Header'
 
 type ModalProps = PropsWithChildren<{
   title: string
@@ -14,37 +14,18 @@ const Modal = ({ children, title }: ModalProps) => {
 
   return (
     <VStack flex={1}>
-      <HStack
-        alignItems='center'
-        borderColor='$primary800'
+      <VStack
         p='$4'
         pt={Platform.OS === 'android' ? insets.top : undefined}
-        justifyContent='space-between'
       >
-        <Link
-          href='../'
-          asChild
-        >
-          <Pressable hitSlop={16}>
-            <Icon
-              as={X}
-              size='xl'
-            />
-          </Pressable>
-        </Link>
-        <Heading size='2xl'>{title}</Heading>
-        <Link
-          href='../'
-          asChild
-        >
-          <Pressable hitSlop={16}>
-            <Icon
-              as={X}
-              size='xl'
-            />
-          </Pressable>
-        </Link>
-      </HStack>
+        <Header
+          left={{
+            href: '../',
+            Icon: X
+          }}
+          title={title}
+        />
+      </VStack>
 
       {children}
     </VStack>
