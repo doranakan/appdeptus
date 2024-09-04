a37f4df5476431a152ca4c458e1be9a9722603be
doranakan <orlandi.m88@gmail.com>
2024-09-04 17:22:40 +0200

    refactor: army svgs

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,54 +1,50 @@
 import adeptaSororitasIcon from './adepta-sororitas.svg'
-import adeptusAstartesIcon from './adeptus-astartes.svg'
+import adeptusCustodesIcon from './adeptus-custodes.svg'
 import adeptusMechanicusIcon from './adeptus-mechanicus.svg'
 import aeldariIcon from './aeldari.svg'
+import agentsOfTheImperiumIcon from './agents-of-the-imperium.svg'
 import astraMilitarumIcon from './astra-militarum.svg'
 import blackTemplarsIcon from './black-templars.svg'
 import bloodAngelsIcon from './blood-angels.svg'
-import bloodRavensIcon from './blood-ravens.svg'
 import chaosDaemonsIcon from './chaos-daemons.svg'
 import chaosSpaceMarinesIcon from './chaos-space-marines.svg'
-import codexSelectionMask from './codex-selection.svg'
 import darkAngelsIcon from './dark-angels.svg'
 import deathGuardIcon from './death-guard.svg'
-import deathwatchIcon from './deathwatch.svg'
 import drukhariIcon from './drukhari.svg'
 import genestealerCultsIcon from './genestealer-cults.svg'
 import greyKnightsIcon from './grey-knights.svg'
-import imperialFistsIcon from './imperial-fists.svg'
-import inquisitionIcon from './inquisition.svg'
+import leaguesOfVotannIcon from './leagues-of-votann.svg'
 import necronsIcon from './necrons.svg'
 import orksIcon from './orks.svg'
+import spaceMarinesIcon from './space-marines.svg'
 import spaceWolvesIcon from './space-wolves.svg'
-import tauIcon from './tau.svg'
+import tauEmpireIcon from './tau-empire.svg'
 import thousandSonsIcon from './thousand-sons.svg'
 import tyranidsIcon from './tyranids.svg'
 import worldEatersIcon from './world-eaters.svg'
 
 export {
   adeptaSororitasIcon,
-  adeptusAstartesIcon,
+  adeptusCustodesIcon,
   adeptusMechanicusIcon,
   aeldariIcon,
+  agentsOfTheImperiumIcon,
   astraMilitarumIcon,
   blackTemplarsIcon,
   bloodAngelsIcon,
-  bloodRavensIcon,
   chaosDaemonsIcon,
   chaosSpaceMarinesIcon,
-  codexSelectionMask,
   darkAngelsIcon,
   deathGuardIcon,
-  deathwatchIcon,
   drukhariIcon,
   genestealerCultsIcon,
   greyKnightsIcon,
-  imperialFistsIcon,
-  inquisitionIcon,
+  leaguesOfVotannIcon,
   necronsIcon,
   orksIcon,
+  spaceMarinesIcon,
   spaceWolvesIcon,
-  tauIcon,
+  tauEmpireIcon,
   thousandSonsIcon,
   tyranidsIcon,
   worldEatersIcon
