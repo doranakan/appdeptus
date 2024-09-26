4ab70b0b64ee370e21507951e76103870cd793f0
doranakan <orlandi.m88@gmail.com>
2024-09-26 22:30:07 +0200

    refactor: trash components

--- components/ArmyBackgroundImage/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AnimatedArmyBackgroundImage } from './AnimatedArmyBackgroundImage'
-export { default as ArmyBackgroundImage } from './ArmyBackgroundImage'
--- components/ArmyIcon/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AnimatedArmyIcon } from './AnimatedArmyIcon'
-export { default as ArmyIcon } from './ArmyIcon'
-export { default as mapCodexNameToIcon } from './mapCodexNameToIcon'
