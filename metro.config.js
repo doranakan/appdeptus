103f8502894e158bd1144bdc1109b1d73a1f0143
doranakan <orlandi.m88@gmail.com>
2024-09-30 22:49:03 +0200

    build: metro config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,18 @@
+const path = require('path')
 const { getDefaultConfig } = require('expo/metro-config')
+
+const { generate } = require('@storybook/react-native/scripts/generate')
 const { withNativeWind } = require('nativewind/metro')
 
+generate({
+  configPath: path.resolve(__dirname, './.storybook')
+})
+
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
+
+config.transformer.unstable_allowRequireContext = true
+
+config.resolver.sourceExts.push('mjs')
 
 module.exports = withNativeWind(config, { input: './global.css' })
