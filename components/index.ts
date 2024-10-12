6a7b3d10c5594df6ec917eefe09fd65e31261d05
doranakan <orlandi.m88@gmail.com>
2024-10-12 12:47:58 +0200

    build: add text component

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,3 @@
+export * from './store'
+export { default as Text } from './Text'
 export * from './ui'
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,5 @@
 export { GluestackUIProvider } from './gluestack-ui-provider'
 export { HStack } from './hstack'
 export { Pressable } from './pressable'
+export { Text as GSText } from './text'
 export { VStack } from './vstack'
