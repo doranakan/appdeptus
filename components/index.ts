d360c8efdfec28bc1d30a75e499c0396a4ea0d9c
doranakan <orlandi.m88@gmail.com>
2024-10-14 20:24:43 +0200

    chore: export components

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,6 @@
-export * from './store'
+export { default as Button } from './Button'
+export { default as Card } from './Card'
 export { default as Text } from './Text'
+
+export * from './store'
 export * from './ui'
