0c59c54519429e577deab3b4c31e088728355e81
doranakan <orlandi.m88@gmail.com>
2024-10-15 17:34:38 +0200

    refactor: replace expo gradient with design system one

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,7 +1,6 @@
-import { LinearGradient } from 'expo-linear-gradient'
 import { type PropsWithChildren } from 'react'
 import InnerBorder from '../InnerBorder'
-import { VStack } from '../ui'
+import { LinearGradient, VStack } from '../ui'
 
 type CardProps = PropsWithChildren
 
@@ -9,7 +8,8 @@ const Card = ({ children }: CardProps) => (
   <VStack className='w-full rounded-2xl bg-primary-700 shadow-lg'>
     <InnerBorder>
       <LinearGradient
-        colors={['rgba(0,0,0,0)', '#2b394f']}
+        className='bg-gradient-to-br from-primary-700 to-primary-900'
+        colors={['bg-primary-700', 'bg-primary-900']}
         start={{ x: 0.3, y: 1 }}
         end={{ x: 1, y: 3 }}
       >
