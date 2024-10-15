98899b6cf1c26966a0620ca4a5c9d99ce668f476
doranakan <orlandi.m88@gmail.com>
2024-10-15 20:23:07 +0200

    refactor: remove linear gradient

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,20 +1,20 @@
 import { type PropsWithChildren } from 'react'
 import InnerBorder from '../InnerBorder'
-import { LinearGradient, VStack } from '../ui'
+import { VStack } from '../ui'
 
 type CardProps = PropsWithChildren
 
 const Card = ({ children }: CardProps) => (
   <VStack className='w-full rounded-2xl bg-primary-700 shadow-lg'>
     <InnerBorder>
-      <LinearGradient
+      {/* <LinearGradient
         className='bg-gradient-to-br from-primary-700 to-primary-900'
         colors={['bg-primary-700', 'bg-primary-900']}
         start={{ x: 0.3, y: 1 }}
         end={{ x: 1, y: 3 }}
       >
-        {children}
-      </LinearGradient>
+      </LinearGradient> */}
+      {children}
     </InnerBorder>
   </VStack>
 )
