b882820f3e4f1a7811937d46e89607a2a1529d5c
doranakan <orlandi.m88@gmail.com>
2024-10-17 09:54:58 +0200

    fix: card gradient

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,20 +1,20 @@
 import { type PropsWithChildren } from 'react'
 import InnerBorder from '../InnerBorder'
-import { VStack } from '../ui'
+import { LinearGradient, VStack } from '../ui'
 
 type CardProps = PropsWithChildren
 
 const Card = ({ children }: CardProps) => (
   <VStack className='w-full rounded-2xl bg-primary-700 shadow-lg'>
     <InnerBorder>
-      {/* <LinearGradient
+      <LinearGradient
         className='bg-gradient-to-br from-primary-700 to-primary-900'
-        colors={['bg-primary-700', 'bg-primary-900']}
+        colors={['rgba(28, 37, 53, 0)', 'rgba(28, 37, 53, 0.6)']}
         start={{ x: 0.3, y: 1 }}
         end={{ x: 1, y: 3 }}
       >
-      </LinearGradient> */}
-      {children}
+        {children}
+      </LinearGradient>
     </InnerBorder>
   </VStack>
 )
