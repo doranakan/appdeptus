914222ef5ea7c55c9a5456134d2ecf7d885c8fb7
doranakan <orlandi.m88@gmail.com>
2024-10-17 23:44:26 +0200

    fix: card border radius

--- components/InnerBorder/InnerBorder.tsx
+++ components/InnerBorder/InnerBorder.tsx
@@ -2,10 +2,10 @@ import { VStack } from 'appdeptus/components/ui'
 import { type PropsWithChildren } from 'react'
 
 const InnerBorder = ({ children }: PropsWithChildren) => (
-  <VStack className='overflow-hidden rounded-2xl'>
+  <VStack className='overflow-hidden rounded-3xl'>
     {children}
     <VStack
-      className='absolute h-full w-full rounded-2xl opacity-20'
+      className='absolute h-full w-full rounded-3xl opacity-20'
       pointerEvents='none'
       style={{ borderWidth: 1, borderColor: '#fff' }}
     />
