004d8c22d1b4506e59c849fac71e53d91727eca1
doranakan <orlandi.m88@gmail.com>
2024-10-17 16:27:09 +0200

    fix: tailwind config for storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import gluestackPlugin from '@gluestack-ui/nativewind-utils/tailwind-plugin'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'media',
-  content: ['./app/**/*.{tsx,jsx,ts,js}', './components/**/*.{tsx,jsx,ts,js}'],
+  content: ['./app/**/*.{tsx,jsx,ts,js}', './components/**/*.{tsx,jsx,ts,js}', './.storybook/**/*.{tsx,jsx,ts,js}'],
   presets: [require('nativewind/preset')],
   safelist: [
     {
