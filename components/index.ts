c85147317420d134bc85a4e616cad543e61e74b8
doranakan <orlandi.m88@gmail.com>
2024-10-17 23:44:49 +0200

    feat: army list item component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export { default as ArmyListItem } from './ArmyListItem'
 export { default as Button } from './Button'
 export { default as Card } from './Card'
 export { default as Text } from './Text'
