80e70cb87f4401a55833bde91cc54d095bf2d31e
doranakan <orlandi.m88@gmail.com>
2024-10-19 17:29:44 +0200

    fix: button color

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,7 +2,9 @@ import clsx from 'clsx'
 import { LinearGradient } from 'expo-linear-gradient'
 import { Link } from 'expo-router'
 import { type LucideIcon } from 'lucide-react-native'
+import { useSelector } from 'react-redux'
 import InnerBorder from '../InnerBorder'
+import { selectThemeName } from '../store'
 import Text from '../Text'
 import { HStack, Icon, Pressable, themeColors, VStack } from '../ui'
 type BaseButton = {
@@ -66,49 +68,52 @@ const ButtonContent = ({
   icon,
   size = 'md',
   text
-}: Omit<ButtonProps, 'variant' | 'link' | 'onPress' | 'disabled'>) => (
-  <VStack className='overflow-hidden'>
-    <InnerBorder
-      rounded={clsx([
-        size === 'md' && 'rounded-2xl',
-        size === 'sm' && 'rounded-xl'
-      ])}
-    >
-      <LinearGradient
-        className='bg-gradient-to-br from-tertiary-600 to-tertiary-800'
-        colors={[
-          themeColors.default[color][600],
-          themeColors.default[color][950]
-        ]}
-        start={{ x: 0.3, y: 1 }}
-        end={{ x: 1, y: 3 }}
+}: Omit<ButtonProps, 'variant' | 'link' | 'onPress' | 'disabled'>) => {
+  const themeName = useSelector(selectThemeName)
+  return (
+    <VStack className='overflow-hidden'>
+      <InnerBorder
+        rounded={clsx([
+          size === 'md' && 'rounded-2xl',
+          size === 'sm' && 'rounded-xl'
+        ])}
       >
-        <HStack
-          className={clsx([
-            'items-center',
-            size === 'md' && 'p-3',
-            size === 'sm' && 'px-2 py-1'
-          ])}
-          space='md'
+        <LinearGradient
+          className='bg-gradient-to-br from-tertiary-600 to-tertiary-800'
+          colors={[
+            themeColors[themeName][color][600],
+            themeColors[themeName][color][950]
+          ]}
+          start={{ x: 0.3, y: 1 }}
+          end={{ x: 1, y: 3 }}
         >
-          {icon ? (
-            <Icon
-              className='color-primary-50'
-              as={icon}
-              size='xl'
-            />
-          ) : null}
-          {text ? (
-            <Text
-              className='uppercase'
-              family='body-bold'
-            >
-              {text}
-            </Text>
-          ) : null}
-        </HStack>
-      </LinearGradient>
-    </InnerBorder>
-  </VStack>
-)
+          <HStack
+            className={clsx([
+              'items-center',
+              size === 'md' && 'p-3',
+              size === 'sm' && 'px-2 py-1'
+            ])}
+            space='md'
+          >
+            {icon ? (
+              <Icon
+                className='color-primary-50'
+                as={icon}
+                size='xl'
+              />
+            ) : null}
+            {text ? (
+              <Text
+                className='uppercase'
+                family='body-bold'
+              >
+                {text}
+              </Text>
+            ) : null}
+          </HStack>
+        </LinearGradient>
+      </InnerBorder>
+    </VStack>
+  )
+}
 export default Button
