e8731bd2e60739635c360051182120754b61bb54
doranakan <orlandi.m88@gmail.com>
2024-10-19 10:20:37 +0200

    feat: navigation header

---
+++ components/NavigationHeader/NavigationHeader.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import NavigationHeader from './NavigationHeader'
+
+const NavigationHeaderMeta: Meta<typeof NavigationHeader> = {
+  title: 'NavigationHeader',
+  component: NavigationHeader,
+  args: {
+    variant: 'backButton'
+  }
+}
+
+export default NavigationHeaderMeta
+
+type Story = StoryObj<typeof NavigationHeader>
+
+export const Default: Story = {}
+export const Avatar: Story = {
+  args: {
+    variant: 'avatar',
+    user: {
+      name: 'doranakan',
+      image:
+        'https://cdnb.artstation.com/p/assets/images/images/025/662/505/large/johannes-helgeson-horusver02-06.jpg?1586525189'
+    }
+  }
+}
+export const RightButton: Story = {
+  args: {
+    rightButton: {
+      href: '../',
+      disabled: true
+    }
+  }
+}
+export const Progress: Story = {
+  args: {
+    ...RightButton.args,
+    progress: {
+      currentStep: 2,
+      steps: 3,
+      text: 'progress'
+    }
+  }
+}
---
+++ components/NavigationHeader/NavigationHeader.tsx
@@ -0,0 +1,60 @@
+import { type UserProfile } from 'appdeptus/models'
+import { ChevronLeft, ChevronRight } from 'lucide-react-native'
+import { type ComponentProps } from 'react'
+import Avatar from '../Avatar'
+import Button from '../Button'
+import Progress from '../Progress'
+import { HStack } from '../ui'
+
+type NavigationHeaderProps = {
+  progress?: ComponentProps<typeof Progress>
+  rightButton?: {
+    href: string
+    icon?: ComponentProps<typeof Button>['icon']
+    disabled?: ComponentProps<typeof Button>['disabled']
+  }
+} & (
+  | {
+      variant: 'backButton'
+    }
+  | {
+      variant: 'avatar'
+      user: UserProfile
+    }
+)
+
+const NavigationHeader = ({
+  progress,
+  rightButton,
+  ...props
+}: NavigationHeaderProps) => (
+  <HStack
+    className='w-full items-center justify-between'
+    space='md'
+  >
+    {/* left items */}
+    {props.variant === 'avatar' ? <Avatar {...props} /> : null}
+    {props.variant === 'backButton' ? (
+      <Button
+        color='primary'
+        variant='link'
+        href='../'
+        icon={ChevronLeft}
+      />
+    ) : null}
+
+    {/* center items */}
+    {progress ? <Progress {...progress} /> : null}
+
+    {/* right items */}
+    {rightButton ? (
+      <Button
+        {...rightButton}
+        variant='link'
+        icon={rightButton.icon ?? ChevronRight}
+      />
+    ) : null}
+  </HStack>
+)
+
+export default NavigationHeader
