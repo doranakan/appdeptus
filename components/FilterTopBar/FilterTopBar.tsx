14636a6378147416e8b88c99c01bfa4bc3e3bf06
doranakan <orlandi.m88@gmail.com>
2024-11-01 17:49:45 +0100

    feat: add filter top bar component

---
+++ components/FilterTopBar/FilterTopBar.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx'
+import { ScrollView } from 'react-native'
+import Text from '../Text'
+import { HStack, Pressable, VStack } from '../ui'
+
+type FilterTopBarProps<T extends string> = {
+  onPress: (value: T) => void
+  selectedValue: T
+  values: T[]
+}
+
+const FilterTopBar = <T extends string>({
+  onPress,
+  selectedValue,
+  values
+}: FilterTopBarProps<T>) => (
+  <VStack>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={{ overflow: 'visible' }}
+    >
+      <HStack space='3xl'>
+        {values.map((value) => (
+          <Pressable
+            key={value}
+            className={clsx(
+              selectedValue === value ? 'opacity-100' : 'opacity-60'
+            )}
+            onPress={() => {
+              onPress(value)
+            }}
+          >
+            <Text
+              className='uppercase'
+              family='body-bold'
+              size='lg'
+            >
+              {value}
+            </Text>
+          </Pressable>
+        ))}
+      </HStack>
+    </ScrollView>
+  </VStack>
+)
+
+export default FilterTopBar
