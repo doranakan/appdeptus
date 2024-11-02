a0d3263711850843ab424b3faffbf6ca38681d69
doranakan <orlandi.m88@gmail.com>
2024-11-02 22:41:51 +0100

    feat: dummy unit selection screen

---
+++ modules/armies/screens/UnitSelection/UnitSelection.tsx
@@ -0,0 +1,9 @@
+import { ScreenContainer, Text } from 'appdeptus/components'
+
+const UnitSelectionScreen = () => (
+  <ScreenContainer className='items-center justify-center'>
+    <Text>Unit selection</Text>
+  </ScreenContainer>
+)
+
+export default UnitSelectionScreen
