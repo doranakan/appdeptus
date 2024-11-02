2a6fbe61c9c9ca4325b54419469de613f6f21cb3
doranakan <orlandi.m88@gmail.com>
2024-11-02 18:18:24 +0100

    feat: detachment selection screen

--- modules/armies/screens/DetachmentSelection/DetachmentSelection.tsx
+++ modules/armies/screens/DetachmentSelection/DetachmentSelection.tsx
@@ -1,9 +1,47 @@
-import { ScreenContainer, Text } from 'appdeptus/components'
+import { useUnmount } from 'ahooks'
+import {
+  ScreenContainer,
+  ScreenTitle,
+  Text,
+  VStack
+} from 'appdeptus/components'
+import { type NewArmy } from 'appdeptus/models'
+import { useFormContext } from 'react-hook-form'
+import { ArmyBuilderBackground } from '../../components'
+import DetachmentList from './DetachmentList'
+import Header from './Header'
 
-const DetachmentSelectionScreen = () => (
-  <ScreenContainer className='items-center justify-center'>
-    <Text>DetachmentSelection</Text>
-  </ScreenContainer>
-)
+const DetachmentSelectionScreen = () => {
+  const { watch, reset } = useFormContext<NewArmy>()
+
+  const selectedCodex = watch('codex')
+
+  useUnmount(() => {
+    reset({
+      codex: selectedCodex
+    })
+  })
+
+  return (
+    <ScreenContainer safeAreaInsets={['bottom', 'top']}>
+      <ArmyBuilderBackground />
+      <VStack
+        className='flex-1 px-4 pb-0 pt-4'
+        space='md'
+      >
+        <Header />
+        <ScreenTitle>{selectedCodex.name}</ScreenTitle>
+        <Text
+          className='uppercase text-primary-200'
+          family='body-bold'
+          size='lg'
+        >
+          detachments
+        </Text>
+        <DetachmentList />
+      </VStack>
+    </ScreenContainer>
+  )
+}
 
 export default DetachmentSelectionScreen
---
+++ modules/armies/screens/DetachmentSelection/Header.tsx
@@ -0,0 +1,28 @@
+import { NavigationHeader } from 'appdeptus/components'
+import { type NewArmy } from 'appdeptus/models'
+import { ChevronRight } from 'lucide-react-native'
+import { useWatch } from 'react-hook-form'
+
+const Header = () => {
+  const watch = useWatch<NewArmy>()
+
+  return (
+    <NavigationHeader
+      variant='backButton'
+      progress={{
+        currentStep: watch.composition?.detachment ? 4 : 3,
+        steps: 10,
+        text: watch.composition?.detachment?.name
+          ? `selected: ${watch.composition?.detachment?.name}`
+          : 'select detachment'
+      }}
+      rightButton={{
+        href: 'army-builder/composition-selection',
+        disabled: !watch.composition?.detachment,
+        icon: ChevronRight
+      }}
+    />
+  )
+}
+
+export default Header
