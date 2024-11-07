d661b24924252c0791b3853c4f82de55d0fde4a4
doranakan <orlandi.m88@gmail.com>
2024-11-07 10:58:28 +0100

    chore: dummy warlord selection screen

---
+++ app/army-builder/warlord-selection.tsx
@@ -0,0 +1,5 @@
+import { WarlordSelectionScreen } from 'appdeptus/modules/armies/screens'
+
+const WarlordSelectionRoute = () => <WarlordSelectionScreen />
+
+export default WarlordSelectionRoute
---
+++ modules/armies/screens/WarlordSelection/WarlordSelection.tsx
@@ -0,0 +1,9 @@
+import { ScreenContainer, Text } from 'appdeptus/components'
+
+const WarlordSelectionScreen = () => (
+  <ScreenContainer className='items-center justify-center'>
+    <Text>Warlord selection</Text>
+  </ScreenContainer>
+)
+
+export default WarlordSelectionScreen
