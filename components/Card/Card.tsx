61321aa3bfd75a0118a22afb0916a959f2e6a52c
doranakan <orlandi.m88@gmail.com>
2024-11-09 12:06:45 +0100

    feat: card with blur mode

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,22 +1,29 @@
 import clsx from 'clsx'
+import { BlurView } from 'expo-blur'
 import { LinearGradient } from 'expo-linear-gradient'
 import { memo, type PropsWithChildren } from 'react'
+import { Platform } from 'react-native'
 import { useSelector } from 'react-redux'
 import InnerBorder from '../InnerBorder'
 import { selectThemeName } from '../store'
 import { themeColors, VStack } from '../ui'
 
 type CardProps = PropsWithChildren<{
+  mode?: 'blur' | 'gradient'
   variant?: 'default' | 'disabled' | 'selectable' | 'selected'
 }>
 
-const Card = ({ children, variant = 'default' }: CardProps) => {
+const Card = ({
+  children,
+  mode = 'gradient',
+  variant = 'default'
+}: CardProps) => {
   const themeName = useSelector(selectThemeName)
 
   return (
     <VStack
       className={clsx([
-        'w-full rounded-3xl bg-primary-700',
+        'w-full rounded-3xl bg-primary-700/10',
         variantToShadow[variant]
       ])}
     >
@@ -24,16 +31,25 @@ const Card = ({ children, variant = 'default' }: CardProps) => {
         opacity={variant === 'selected' ? 'opacity-40' : undefined}
         selected={variant === 'selected'}
       >
-        <LinearGradient
-          colors={[
-            themeColors[themeName].primary[700],
-            themeColors[themeName].primary[950]
-          ]}
-          start={{ x: 0.3, y: 1 }}
-          end={{ x: 1, y: 3 }}
-        >
-          {children}
-        </LinearGradient>
+        {mode === 'gradient' ? (
+          <LinearGradient
+            colors={[
+              themeColors[themeName].primary[700],
+              themeColors[themeName].primary[950]
+            ]}
+            start={{ x: 0.3, y: 1 }}
+            end={{ x: 1, y: 3 }}
+          >
+            {children}
+          </LinearGradient>
+        ) : (
+          <BlurView
+            intensity={Platform.OS === 'android' ? 40 : 10}
+            tint='regular'
+          >
+            {children}
+          </BlurView>
+        )}
         {variant === 'disabled' ? (
           <VStack
             className='absolute h-full w-full bg-primary-950/70'
