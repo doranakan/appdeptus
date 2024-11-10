9bd627700fb313e07ce229ca34715d574258b893
doranakan <orlandi.m88@gmail.com>
2024-11-10 17:42:11 +0100

    feat: army editing

--- modules/armies/screens/DetachmentSelection/DetachmentSelection.tsx
+++ modules/armies/screens/DetachmentSelection/DetachmentSelection.tsx
@@ -1,7 +1,11 @@
+import { skipToken } from '@reduxjs/toolkit/query'
 import { useUnmount } from 'ahooks'
 import { ScreenContainer, VStack } from 'appdeptus/components'
 import { type ArmyBuilder } from 'appdeptus/models'
+import { useLocalSearchParams } from 'expo-router'
+import { useEffect } from 'react'
 import { useFormContext } from 'react-hook-form'
+import { useGetArmyListQuery } from '../../api'
 import { ArmyBuilderBackground, TopBar } from '../../components'
 import DetachmentList from './DetachmentList'
 import Header from './Header'
@@ -9,7 +13,54 @@ import Header from './Header'
 const DetachmentSelectionScreen = () => {
   const { watch, reset, getValues } = useFormContext<ArmyBuilder>()
 
+  const { id } = useLocalSearchParams<{ id: string }>()
+
   const selectedCodex = watch('codex')
+
+  const { army } = useGetArmyListQuery(!id ? skipToken : undefined, {
+    selectFromResult: ({ data, ...rest }) => ({
+      army: data?.find(({ id: armyId }) => armyId === Number(id)),
+      ...rest
+    })
+  })
+
+  useEffect(() => {
+    if (army) {
+      reset({
+        codex: army.codex,
+        detachment: army.composition.detachment,
+        id: army.id,
+        name: army.name,
+        points: army.points,
+        warlord: army.composition.warlord,
+        units: [
+          ...army.composition.characters,
+          ...army.composition.leaders,
+          ...army.composition.squads,
+          ...army.composition.teams.flatMap(({ id, leader, bodyguard }) => [
+            { ...leader, teamId: id },
+            { ...bodyguard, teamId: id }
+          ]),
+          ...army.composition.transports,
+          ...army.composition.vehicles,
+          ...[
+            army.composition.warlord.type === 'team'
+              ? [
+                  {
+                    ...army.composition.warlord.leader,
+                    teamId: army.composition.warlord.id
+                  },
+                  {
+                    ...army.composition.warlord.bodyguard,
+                    teamId: army.composition.warlord.id
+                  }
+                ]
+              : army.composition.warlord
+          ].flat()
+        ]
+      })
+    }
+  }, [army, reset])
 
   useUnmount(() => {
     reset({
@@ -29,7 +80,7 @@ const DetachmentSelectionScreen = () => {
         <Header />
         <TopBar
           subtitle='detachments'
-          title={selectedCodex.name}
+          title={selectedCodex?.name}
         />
         <DetachmentList />
       </VStack>
