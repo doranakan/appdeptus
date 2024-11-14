47099f8828b794209a70638ccfb19cf4eb6302ec
doranakan <orlandi.m88@gmail.com>
2024-11-14 09:33:23 +0100

    chore: disable new game button when user has no armies

--- modules/games/screens/Games/Header.tsx
+++ modules/games/screens/Games/Header.tsx
@@ -1,16 +1,20 @@
 import { NavigationHeader } from 'appdeptus/components'
+import { useGetArmyListQuery } from 'appdeptus/modules/armies/api'
 import { useGetUserProfileQuery } from 'appdeptus/modules/user/api'
 import { Swords } from 'lucide-react-native'
 import { memo } from 'react'
 
 const Header = () => {
-  const { data } = useGetUserProfileQuery()
+  const { data: user } = useGetUserProfileQuery()
+
+  const { data: armies } = useGetArmyListQuery()
 
   return (
     <NavigationHeader
       variant='avatar'
-      user={data}
+      user={user}
       rightButton={{
+        disabled: !armies?.length,
         href: 'games/new',
         icon: Swords,
         variant: 'link'
