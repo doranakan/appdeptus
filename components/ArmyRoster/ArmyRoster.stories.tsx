eb0892b13e61a2120c377736167d425551eadcda
doranakan <orlandi.m88@gmail.com>
2024-11-17 10:43:41 +0100

    refactor: extract army roster component

---
+++ components/ArmyRoster/ArmyRoster.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import ArmyRoster from './ArmyRoster'
+
+const ArmyRosterMeta: Meta<typeof ArmyRoster> = {
+  title: 'ArmyRoster',
+  component: ArmyRoster,
+  args: {
+    composition: {
+      characters: [],
+      detachment: {
+        enhancements: [],
+        id: 0,
+        name: ''
+      },
+      leaders: [],
+      teams: [],
+      transports: [],
+      vehicles: [],
+      warlord: {
+        id: 0,
+        name: 'Angron',
+        selectionId: '0',
+        tier: {
+          id: 0,
+          models: 1,
+          points: 435
+        },
+        upgrades: [],
+        type: 'leader'
+      },
+      squads: [
+        {
+          id: 1,
+          name: 'Khorne Berzerkers',
+          selectionId: '1',
+          tier: {
+            id: 1,
+            models: 10,
+            points: 180
+          },
+          upgrades: [],
+          type: 'squad'
+        },
+        {
+          id: 2,
+          name: 'Jakhals',
+          selectionId: '2',
+          tier: {
+            id: 2,
+            models: 20,
+            points: 160
+          },
+          upgrades: [],
+          type: 'squad'
+        }
+      ]
+    }
+  }
+}
+
+export default ArmyRosterMeta
+
+type Story = StoryObj<typeof ArmyRoster>
+
+export const Default: Story = {}
---
+++ components/ArmyRoster/ArmyRoster.tsx
@@ -0,0 +1,58 @@
+import { type Army } from 'appdeptus/models'
+import React, { type ComponentProps, memo, useMemo } from 'react'
+import { FlatList } from 'react-native'
+import { VStack } from '../ui'
+import UnitListItem from '../UnitListItem'
+
+type UnitRosterProps = {
+  composition: Army['composition']
+  ListHeaderComponent?: ComponentProps<typeof FlatList>['ListHeaderComponent']
+}
+
+const UnitRoster = ({ composition, ListHeaderComponent }: UnitRosterProps) => {
+  const units = useMemo(() => {
+    const {
+      characters,
+      leaders,
+      squads,
+      teams,
+      transports,
+      vehicles,
+      warlord
+    } = composition
+
+    return [
+      warlord,
+      ...teams,
+      ...characters,
+      ...leaders,
+      ...squads,
+      ...transports,
+      ...vehicles
+    ]
+  }, [composition])
+
+  return (
+    <FlatList
+      data={units}
+      showsVerticalScrollIndicator={false}
+      ItemSeparatorComponent={() => <VStack className='h-4' />}
+      keyExtractor={(unit) => {
+        if (unit.type !== 'team') {
+          return unit.selectionId
+        }
+        return unit.id
+      }}
+      ListFooterComponent={() => <VStack className='h-4' />}
+      ListHeaderComponent={ListHeaderComponent}
+      renderItem={({ item, index }) => (
+        <UnitListItem
+          unitOrTeam={item}
+          warlord={index === 0}
+        />
+      )}
+    />
+  )
+}
+
+export default memo(UnitRoster)
