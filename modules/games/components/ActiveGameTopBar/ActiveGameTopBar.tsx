5183474ae5f4427327d884ee1b4af66b4042dfd6
doranakan <orlandi.m88@gmail.com>
2024-12-02 16:10:21 +0100

    chore: remove console logs

--- modules/games/components/ActiveGameTopBar/ActiveGameTopBar.tsx
+++ modules/games/components/ActiveGameTopBar/ActiveGameTopBar.tsx
@@ -18,8 +18,6 @@ import { useGetGameQuery } from '../../api'
 const ActiveGameTopBar = () => {
   const { data: game, refetch } = useGetGameQuery()
 
-  console.log('asd')
-
   useFocusEffect(
     useCallback(() => {
       refetch()
