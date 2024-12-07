6e3a5dff21ad89baee40bd59cb92081f932a6e2d
doranakan <orlandi.m88@gmail.com>
2024-12-07 17:50:53 +0100

    feat: privacy link component

---
+++ modules/root/components/PrivacyLink/PrivacyLink.tsx
@@ -0,0 +1,9 @@
+import { Text, TextLink } from 'appdeptus/components'
+
+const PrivacyLink = () => (
+  <Text>
+    I accept the <TextLink href='privacy-policy'>privacy policy</TextLink>
+  </Text>
+)
+
+export default PrivacyLink
---
+++ modules/root/components/index.ts
@@ -0,0 +1,1 @@
+export { default as PrivacyLink } from './PrivacyLink'
