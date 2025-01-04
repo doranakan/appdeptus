40f96606587bc2f986d089cef0a6f02bfa6ec344
doranakan <orlandi.m88@gmail.com>
2025-01-04 18:38:05 +0100

    chore: adjust disclaimer copy 📇

--- components/Disclaimer/Disclaimer.tsx
+++ components/Disclaimer/Disclaimer.tsx
@@ -6,11 +6,9 @@ const Disclaimer = () => (
     players, designed to assist with roster building, game scores, and points
     tracking. It is not affiliated with or endorsed by Games Workshop. The app
     does not use any copyrighted images, logos, or assets from Games Workshop,
-    and names are included solely to support gameplay. Appdeptus is completely
+    and any name used is solely to support gameplay. Appdeptus is completely
     free to use, with no purchases or subscriptions, and is intended purely for
-    entertainment. All data is maintained and updated by the community.
-    Warhammer 40,000 and related trademarks are the property of Games Workshop.
-    For official resources, please visit their website.
+    entertainment.
   </Text>
 )
 
