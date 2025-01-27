0b9811baa0eb9a091a326f446b84c2bffdda5281
Luca Caputo <caputo.luca@outlook.com>
2025-01-27 18:02:37 +0100

    refactor: add minor props to screen title for better customization

--- components/ScreenTitle/ScreenTitle.tsx
+++ components/ScreenTitle/ScreenTitle.tsx
@@ -3,12 +3,15 @@ import Text from '../Text'
 
 type ScreenTitleProps = {
   children: string
+  className?: string
 }
 
-const ScreenTitle = ({ children }: ScreenTitleProps) => (
+const ScreenTitle = ({ children, className }: ScreenTitleProps) => (
   <Text
     family='heading-regular'
     size='4xl'
+    numberOfLines={1}
+    className={className}
   >
     {children}
   </Text>
