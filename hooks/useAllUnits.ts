9fc9e749021c9d19a574bc784db7af9f42a314a5
doranakan <orlandi.m88@gmail.com>
2025-02-03 18:51:19 +0100

    refactor: extract roster to unit mapper

--- hooks/useAllUnits.ts
+++ hooks/useAllUnits.ts
@@ -1,35 +1,8 @@
-import {
-  type Army,
-  type GameArmy,
-  type GameUnit,
-  type Unit
-} from 'appdeptus/models'
+import { type Army, type GameArmy } from 'appdeptus/models'
+import { mapRosterToUnits } from 'appdeptus/utils'
 import { useMemo } from 'react'
 
 const useAllUnits = (units: Army['roster'] | GameArmy['roster']) =>
-  useMemo(
-    () =>
-      units.reduce<(Unit | GameUnit)[]>((acc, curr) => {
-        switch (curr.type) {
-          case 'embarked':
-            return [
-              ...acc,
-              curr.transport,
-              ...curr.crew.flatMap((embarked) =>
-                embarked.type === 'team'
-                  ? [embarked.leader, embarked.bodyguard]
-                  : embarked
-              )
-            ]
-
-          case 'team':
-            return [...acc, curr.leader, curr.bodyguard]
-
-          default:
-            return [...acc, curr]
-        }
-      }, []),
-    [units]
-  )
+  useMemo(() => mapRosterToUnits(units), [units])
 
 export default useAllUnits
--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,3 @@
 export { default as mapNullToUndefined } from './mapNullToUndefined'
+export { default as mapRosterToUnits } from './mapRosterToUnits'
 export { default as supabase } from './supabase'
