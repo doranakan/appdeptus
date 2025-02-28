fffea84b8dba497bae6fa185870251a1d572d16b
Luca Caputo <caputo.luca@outlook.com>
2025-02-28 15:37:08 +0100

    --wip-- [skip ci]

--- modules/armies/screens/CodexSelection/CodexListItem.tsx
+++ modules/armies/screens/CodexSelection/CodexListItem.tsx
@@ -6,6 +6,7 @@ import {
   VStack
 } from 'appdeptus/components'
 import { type Codex } from 'appdeptus/models'
+import { useThemeSwitch } from 'appdeptus/providers/ThemeSwitchProvider/ThemeSwitchContext'
 import clsx from 'clsx'
 import { memo } from 'react'
 
@@ -16,37 +17,39 @@ type CodexListItemProps = {
   selected?: boolean
 }
 
-const CodexListItem = ({ codex, onPress, selected }: CodexListItemProps) => (
-  <Pressable
-    className='active:opacity-80'
-    onPress={() => {
-      onPress(codex)
-    }}
-  >
-    <Card
-      variant={
-        selected === undefined
+const CodexListItem = ({ codex, onPress, selected }: CodexListItemProps) => {
+  const { switchTheme } = useThemeSwitch()
+  return (
+    <Pressable
+      className='active:opacity-80'
+      onPress={({ nativeEvent: { pageX, pageY } }) => {
+        onPress(codex)
+        switchTheme(codex.name, { x: pageX, y: pageY })
+      } }
+    >
+      <Card
+        variant={selected === undefined
           ? 'selectable'
           : selected
             ? 'selected'
-            : 'selectable-alt'
-      }
-    >
-      <VStack
-        className={clsx(['absolute h-full w-full', selected && 'opacity-20'])}
+            : 'selectable-alt'}
       >
-        <ArmyBackground codex={codex.name} />
-      </VStack>
-      <VStack className='p-4'>
-        <Text
-          family='body-bold'
-          size='lg'
+        <VStack
+          className={clsx(['absolute h-full w-full', selected && 'opacity-20'])}
         >
-          {codex.name}
-        </Text>
-      </VStack>
-    </Card>
-  </Pressable>
-)
+          <ArmyBackground codex={codex.name} />
+        </VStack>
+        <VStack className='p-4'>
+          <Text
+            family='body-bold'
+            size='lg'
+          >
+            {codex.name}
+          </Text>
+        </VStack>
+      </Card>
+    </Pressable>
+  )
+}
 
 export default memo(CodexListItem)
