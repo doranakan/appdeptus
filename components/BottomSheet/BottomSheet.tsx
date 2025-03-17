51d66fab847544f30ff1ab4911b76cb540366106
doranakan <orlandi.m88@gmail.com>
2025-03-17 20:55:45 +0100

    refactor: bottom sheet

--- components/BottomSheet/BottomSheet.tsx
+++ components/BottomSheet/BottomSheet.tsx
@@ -1,18 +1,13 @@
-import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet'
+import { BottomSheetModal, BottomSheetScrollView } from '@gorhom/bottom-sheet'
 import { type BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types'
-import clsx from 'clsx'
-import { BlurView } from 'expo-blur'
 import {
   forwardRef,
   type ForwardRefRenderFunction,
   memo,
   type PropsWithChildren
 } from 'react'
-import { StyleSheet, useWindowDimensions } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import InnerBorder from '../InnerBorder'
-import { VStack } from '../ui'
+import { StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import Backdrop from './Backdrop'
 
 type BottomSheetProps = {
@@ -24,59 +19,41 @@ type BottomSheetProps = {
 const BottomSheet: ForwardRefRenderFunction<
   BottomSheetModalMethods,
   PropsWithChildren<BottomSheetProps>
-> = ({ children, onDismiss, onPressBackdrop, scrollDisabled }, ref) => {
-  const window = useWindowDimensions()
-
-  const { top } = useSafeAreaInsets()
-
-  return (
-    <BottomSheetModal
-      ref={ref}
-      style={styles.container}
-      backgroundStyle={styles.container}
-      handleComponent={null}
-      detached={false}
-      overDragResistanceFactor={0}
-      maxDynamicContentSize={window.height - top}
-      onDismiss={onDismiss}
-      backdropComponent={(props) => (
-        <Backdrop
-          {...props}
-          onPress={onPressBackdrop}
-        />
-      )}
+> = ({ children, onDismiss, onPressBackdrop, scrollDisabled }, ref) => (
+  <BottomSheetModal
+    ref={ref}
+    style={styles.container}
+    backgroundStyle={styles.container}
+    handleComponent={null}
+    onDismiss={onDismiss}
+    overDragResistanceFactor={5}
+    backdropComponent={(props) => (
+      <Backdrop
+        {...props}
+        onPress={onPressBackdrop}
+      />
+    )}
+  >
+    <BottomSheetScrollView
+      className='overflow-visible'
+      contentContainerClassName='p-4 overflow-visible'
+      showsVerticalScrollIndicator={false}
+      scrollEnabled={!scrollDisabled}
     >
-      <BottomSheetView style={styles.container}>
-        <InnerBorder>
-          <BlurView>
-            <VStack
-              className='overflow-x-visible'
-              space='md'
-            >
-              <VStack
-                className='mt-4 h-2 w-16 self-center rounded-full bg-primary-50'
-                hitSlop={16}
-              />
-              <ScrollView
-                className={clsx('overflow-x-visible px-4')}
-                contentContainerClassName='pb-10'
-                scrollEnabled={!scrollDisabled}
-                showsVerticalScrollIndicator={false}
-              >
-                {children}
-              </ScrollView>
-            </VStack>
-          </BlurView>
-        </InnerBorder>
-      </BottomSheetView>
-    </BottomSheetModal>
-  )
-}
+      <SafeAreaView
+        edges={['bottom', 'top']}
+        style={styles.container}
+      >
+        {children}
+      </SafeAreaView>
+    </BottomSheetScrollView>
+  </BottomSheetModal>
+)
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'transparent',
-    minHeight: 10
+    overflow: 'visible'
   }
 })
 
