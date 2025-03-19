148d8f73d66676e847b6f2ba5bf7ab36127e9461
doranakan <orlandi.m88@gmail.com>
2025-03-19 17:07:53 +0100

    feat: player tag hash

--- components/PlayerTag/PlayerTag.tsx
+++ components/PlayerTag/PlayerTag.tsx
@@ -1,4 +1,5 @@
 import { type UserProfile } from 'appdeptus/models'
+import { snakeCase } from 'lodash'
 import Avatar from '../Avatar'
 import Text from '../Text'
 import { HStack } from '../ui'
@@ -7,19 +8,28 @@ type PlayerTagProps = {
   player: UserProfile
 
   reversed?: boolean
+  showHash?: boolean
 }
 
-const PlayerTag = ({ player, reversed }: PlayerTagProps) => (
+const PlayerTag = ({ player, reversed, showHash }: PlayerTagProps) => (
   <HStack
     className='items-center'
     reversed={reversed}
     space='sm'
   >
     <Avatar
-      user={player}
+      {...player}
       size='sm'
     />
-    <Text family='body-bold'>{`@${player.name.toLowerCase()}`}</Text>
+    <Text family='body-bold'>{`@${snakeCase(player.name)}`}</Text>
+    {showHash ? (
+      <Text
+        className='text-ellipsis text-primary-300'
+        size='xs'
+      >
+        {`#${player.id.split('-')[0]}`}
+      </Text>
+    ) : null}
   </HStack>
 )
 
