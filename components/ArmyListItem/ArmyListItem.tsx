919af71867e092792365d46ba2d4cc57fd9c9be6
doranakan <orlandi.m88@gmail.com>
2025-03-22 11:29:20 +0100

    refactor: add roster owner to army list item

--- components/ArmyListItem/ArmyListItem.tsx
+++ components/ArmyListItem/ArmyListItem.tsx
@@ -1,9 +1,10 @@
-import { type CodexName } from 'appdeptus/models'
+import { type CodexName, type UserProfile } from 'appdeptus/models'
 
 import { memo, type ComponentProps } from 'react'
 import ArmyBackground from '../ArmyBackground'
 import Badge from '../Badge'
 import Card from '../Card'
+import PlayerTag from '../PlayerTag'
 import Text from '../Text'
 import { HStack, VStack } from '../ui'
 
@@ -13,6 +14,7 @@ type ArmyListItemProps = {
   name: string
   points: number
 
+  shareBy?: UserProfile
   isValid?: boolean
   variant?: ComponentProps<typeof Card>['variant']
 }
@@ -24,47 +26,59 @@ const ArmyListItem = ({
   points,
 
   isValid = true,
+  shareBy,
   variant
 }: ArmyListItemProps) => (
   <Card variant={variant}>
-    <ArmyBackground codex={codex} />
-    {!isValid ? (
-      <Text
-        className='rounded-2xl bg-warning-400 px-4 py-1'
-        family='body-bold'
-        size='sm'
+    <VStack className='bg-primary-950 shadow-md'>
+      <ArmyBackground codex={codex} />
+      {!isValid ? (
+        <Text
+          className='rounded-2xl bg-warning-400 px-4 py-1'
+          family='body-bold'
+          size='sm'
+        >
+          💀 Army no longer valid!
+        </Text>
+      ) : null}
+      <VStack
+        className='p-4'
+        space='sm'
       >
-        💀 Army no longer valid!
-      </Text>
-    ) : null}
-    <VStack
-      className='p-4'
-      space='sm'
-    >
-      <HStack className='items-center justify-between'>
-        <Text
-          className='uppercase'
-          family='body-bold'
-          size='lg'
-        >
-          {codex}
-        </Text>
-        <Text
-          className='uppercase'
-          family='body-bold'
-          size='lg'
-        >
-          {`${points}pts`}
-        </Text>
-      </HStack>
-      <HStack className='items-center justify-between'>
-        <Text>{name}</Text>
-        <Badge
-          codex={codex}
-          text={detachment}
+        <HStack className='items-center justify-between'>
+          <Text
+            className='uppercase'
+            family='body-bold'
+            size='lg'
+          >
+            {codex}
+          </Text>
+          <Text
+            className='uppercase'
+            family='body-bold'
+            size='lg'
+          >
+            {`${points}pts`}
+          </Text>
+        </HStack>
+        <HStack className='items-center justify-between'>
+          <Text>{name}</Text>
+          <Badge
+            codex={codex}
+            text={detachment}
+          />
+        </HStack>
+      </VStack>
+    </VStack>
+    {shareBy ? (
+      <HStack className='items-center justify-between bg-primary-950/80 px-4 py-2'>
+        <Text size='sm'>Shared by:</Text>
+        <PlayerTag
+          player={shareBy}
+          reversed
         />
       </HStack>
-    </VStack>
+    ) : null}
     {!isValid ? (
       <VStack className='absolute h-full w-full overflow-hidden rounded-3xl border-2 border-warning-400' />
     ) : null}
