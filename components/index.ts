9519051218fe5207b14ca1ca8688ac5fe75a8f63
Luca Caputo <caputo.luca@outlook.com>
2025-07-31 10:15:09 +0200

    feat: created animated army roster component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export { default as AnimatedArmyRoster } from './AnimatedArmyRoster'
 export { default as ArmyAvatar } from './ArmyAvatar'
 export { default as ArmyBackground } from './ArmyBackground'
 export { default as ArmyListItem } from './ArmyListItem'
