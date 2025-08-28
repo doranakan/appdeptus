52889c946d5674c91490b78dfcb17827fac591c9
Luca Caputo <caputo.luca@outlook.com>
2025-08-28 09:27:43 +0200

    feat: animation tweaks and haptic feedback integration

--- modules/armies/hooks/useArmyRosterBackgroundAnimation.ios.ts
+++ modules/armies/hooks/useArmyRosterBackgroundAnimation.ios.ts
@@ -4,9 +4,13 @@ import {
   useDerivedValue,
   interpolate,
   Extrapolation,
-  useAnimatedScrollHandler
+  useAnimatedScrollHandler,
+  useAnimatedReaction,
+  runOnJS
 } from 'react-native-reanimated'
 import { type ArmyRosterBackgroundAnimationObject } from './types'
+import { useMemoizedFn } from 'ahooks'
+import * as Haptics from 'expo-haptics'
 
 const useArmyRosterBackgroundAnimation =
   (): ArmyRosterBackgroundAnimationObject => {
@@ -20,6 +24,23 @@ const useArmyRosterBackgroundAnimation =
         scrollValue.value = event.contentOffset.y
       }
     })
+
+    const impact = useMemoizedFn((style: Haptics.ImpactFeedbackStyle) => {
+      Haptics.impactAsync(style)
+    })
+
+    useAnimatedReaction(
+      () => scrollValue.value,
+      (curr, prev) => {
+        if (curr === 200) {
+          runOnJS(impact)(Haptics.ImpactFeedbackStyle.Medium)
+          return
+        }
+        if (curr < (prev ?? 0) && prev === 200) {
+          runOnJS(impact)(Haptics.ImpactFeedbackStyle.Medium)
+        }
+      }
+    )
 
     return useMemo(
       () => ({
